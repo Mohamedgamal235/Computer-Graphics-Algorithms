50c847411b529580785b85259dc4be42224c1088
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-14 20:47:04 +0300

    Make Draw Bezier Curve Algorithm

--- DrawBezierCurve.cpp
+++ DrawBezierCurve.cpp
@@ -14,20 +14,55 @@ COLORREF startColor = RGB(255, 0, 0); // -> Red
 COLORREF endColor = RGB(0, 0, 255); // -> Blue
 
 BYTE LinearInterpolation(BYTE a , BYTE b , double t) {
-
+    return (BYTE)(a + (b - a) * t);
 }
 
 COLORREF InterpolationColor(COLORREF c1 , COLORREF c2 , double t) {
-
+    BYTE r = LinearInterpolation(GetRValue(c1), GetRValue(c2), t);
+    BYTE g = LinearInterpolation(GetGValue(c1), GetGValue(c2), t);
+    BYTE b = LinearInterpolation(GetBValue(c1), GetBValue(c2), t);
+    return RGB(r, g, b);
 }
 
 Point BezierEquation(Point p0, Point p1, Point p2, Point p3, double t) {
+    // (1-t)^3p0 + 3(1-t)^2*tp1 + 3(1-t)*t^2*p2 + t^3 * p3
 
+    double u = 1 - t;
+    double u3 = u * u * u;
+    double u2 = u * u;
+    double t2 = t * t;
+    double t3 = t * t * t;
+
+    Point res;
+
+    res.x = (u3 * p0.x) + (3 * u2 * t * p1.x) + (3 * u * t2 * p2.x) + (t3 * p3.x);
+    res.y = (u3 * p0.y) + (3 * u2 * t * p1.y) + (3 * u * t2 * p2.y) + (t3 * p3.y);
+
+    return res;
 }
 
 
 void DrawBezierCurve(HDC hdc, Point p0, Point p1, Point p2, Point p3, COLORREF c1, COLORREF c2, int numOfPoints = 300) {
+    if (numOfPoints < 2)
+        return;
 
+    Point prev = BezierEquation(p0, p1, p2, p3, 0); // start point at t = 0
+    for (int i = 1; i <= numOfPoints; i++) {
+        double t = (double)i / numOfPoints;
+        Point curr = BezierEquation(p0, p1, p2, p3, t);
+        COLORREF col = LinearInterpolation(c1, c2, t);
+
+        HPEN pen = CreatePen(PS_SOLID, 1, col);
+        HGDIOBJ oldPen = SelectObject(hdc, pen);
+
+        MoveToEx(hdc, (int)prev.x, (int)prev.y, NULL);
+        LineTo(hdc, (int)curr.x, (int)curr.y);
+
+        SelectObject(hdc, oldPen);
+        DeleteObject(pen);
+
+        prev = curr;
+    }
 }
 
 
